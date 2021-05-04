dc1670a385c808eb3383806afd981e5331d445c1
Ubuntu <ubuntu@ip-172-31-10-161.ap-south-1.compute.internal>
2021-05-04 06:35:57 +0000

    Initial Commit

---
+++ yogeshdyes/api.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+import frappe
+import frappe.defaults
+from frappe import _
+from frappe import msgprint, _
+from frappe.utils import nowdate, flt, cint, cstr,now_datetime,getdate, add_days, add_months, get_last_day
+
+def validate_user_permission(self,method):
+	if self.user == frappe.session.user:
+		frappe.throw("You cannot change permission for your own user id")
